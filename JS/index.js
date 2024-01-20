ea45473030e1e72a44b7bdb179f9d21f66958ed3
Kacper <wasiak.kacper11@gmail.com>
2024-01-20 16:25:39 +0100

    Aplikacja pokazuje pogodę, wykonałem ikony danej pogody

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
 
-
 /* City name w nagłówku */
 
 document.getElementById("search").addEventListener('click', ()=>{
@@ -9,9 +8,26 @@ document.getElementById("search").addEventListener('click', ()=>{
     }
     else{
         document.getElementById("city-name").innerHTML = cityName.toUpperCase();
-            document.getElementById("city-name-search").value = "";
+            
+            async function showWeather(){
+                let cityName = document.getElementById("city-name-search").value;
+
+                const apiKey = "a6bf352259ef900f8c2c2ebf968546ec";
+                const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+                const result = document.getElementById("result");
+                
+                const response = await fetch(apiURL+`&q=${cityName}&appid=${apiKey}`);
+                var data = await response.json();
+
+                console.log(data);
+                document.getElementById("temp").innerHTML = "Temperatura: "+data.main.temp+"°C";
+                document.getElementById("wind").innerHTML = "Prędkość wiatru: "+data.wind.speed+"km/h";
+                document.getElementById("humidity").innerHTML = "Wilgotność: "+data.main.humidity+"%";
+            }  
+
             showWeather();
-    }
+            document.getElementById("city-name-search").value = "";
+}
 });
 
 document.addEventListener('keypress', (e)=>{
@@ -22,8 +38,26 @@ document.addEventListener('keypress', (e)=>{
         }
         else{
             document.getElementById("city-name").innerHTML = cityName.toUpperCase();
-            document.getElementById("city-name-search").value = "";
+            async function showWeather(){
+                let cityName = document.getElementById("city-name-search").value;
+
+                const apiKey = "a6bf352259ef900f8c2c2ebf968546ec";
+                const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+                const result = document.getElementById("result");
+                
+                const response = await fetch(apiURL+`&q=${cityName}&appid=${apiKey}`);
+                var data = await response.json();
+
+                console.log(data);
+                document.getElementById("temp").innerHTML = "Temperatura: "+data.main.temp+"°C";
+                document.getElementById("wind").innerHTML = "Prędkość wiatru: "+data.wind.speed+"km/h";
+                document.getElementById("humidity").innerHTML = "Wilgotność: "+data.main.humidity+"%";]
+/* 
+                const weather = data. */
+            }  
+
             showWeather();
+            document.getElementById("city-name-search").value = "";
         }
     }
 });
